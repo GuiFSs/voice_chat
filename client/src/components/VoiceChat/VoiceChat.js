adb84f232923f30e63082687a4cc2123b5b68b4d
GuiFSs <devguisantos@gmail.com>
2019-05-02 02:22:20 -0300

    ah...

--- client/src/components/VoiceChat/VoiceChat.js
+++ client/src/components/VoiceChat/VoiceChat.js
@@ -25,10 +25,10 @@ class VoiceChat extends Component {
   connectToVoiceChat = () => {
     const { socket } = this.state;
     const myPeer = new Peer({
-      // key: 'peerjs',
+      // key
       path: '/peerjs', // <==========
       host: 'audio-chat-aps.herokuapp.com',
-      port: 80
+      secure: true
     });
     myPeer.on('open', id => {
       socket.emit('add new peer', id);
